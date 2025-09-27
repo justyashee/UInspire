a7003bbe4108510bd081a9f70a24d01580701fd6
Aboutme2005 <mishramahek88@gmail.com>
2025-09-27 10:13:06 +0530

    new

---
+++ frontend/src/app/components/Footer.jsx
@@ -0,0 +1,13 @@
+// 'use client';
+
+// import React from 'react';
+
+// const Footer = () => {
+//   return (
+//     <footer className="w-full bg-white mt-auto shadow-inner py-4 text-center text-gray-500">
+//       © {new Date().getFullYear()} UI Generator. All rights reserved.
+//     </footer>
+//   );
+// };
+
+// export default Footer;
